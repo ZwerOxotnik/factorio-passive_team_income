953320fc334485186ac6b7d25e7dcd92d8f011f0
ZwerOxotnik <zweroxotnik@gmail.com>
2021-12-22 22:47:01 +0300

    Update to 0.8.5

--- control.lua
+++ control.lua
@@ -1,1 +1,1 @@
-require("event_handler").add_lib(require("models/passive_team_income"))
+require("__zk-lib__/static-libs/lualibs/event_handler_vZO.lua").add_lib(require("models/passive_team_income"))
